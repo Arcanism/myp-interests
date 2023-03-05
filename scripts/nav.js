7bece8b9f95ebbdb2a5cd543654048aa7ec8a7bd
Arcanism <116792065+Arcanism@users.noreply.github.com>
2023-03-05 14:50:44 +0530

    Add files via upload

---
+++ scripts/nav.js
@@ -0,0 +1,20 @@
+
+page = document.getElementById("page");
+        function navhome() {
+            page.src = "home.html"
+        }
+        function navone() {
+            page.src = "sports.html"
+        }
+        function navtwo() {
+            page.src = "books.html"
+        }
+        function navthree() {
+            page.src = "music.html"
+        }
+        function navfour() {
+            page.src = "gaming.html"
+        }
+        function navfive() {
+            page.src = "tech.html"
+        }
